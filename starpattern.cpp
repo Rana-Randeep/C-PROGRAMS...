9a7f37fde0bdf37f2d0d1475616c0b56b91ececb
Randeep Rana <ranarandeep521@gmail.com>
2024-09-10 20:21:11 +0530

    Add files via upload

---
+++ starpattern.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int j, n;
+    cout<<"enter number";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        for(int j=2;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(int j=i;j>=1;j--){
+            cout<<"*";
+        }
+        for(int j=i-1;j>=1;j--){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
